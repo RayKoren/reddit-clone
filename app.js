b224426a6ebda8c03117fe1675018c45b5c7ebe3
rayk216 <raykoren@gmail.com>
2016-07-21 13:44:40 -0600

    votes working

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var app = angular.module("reddit", []);
-
+// $scope.sortType = "votes";
 app.controller("posts", function($scope){
 $scope.posts = [
-  {title: 'Cool Car 1',
+  {title: 'Cool Car',
   author: 'Ray',
   image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
   votes: 5,
-  description:  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  description:  "AUDI"
 },
-  {title: 'Cool Car 2',
-  author: 'Ray',
-  image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
+  {title: 'Another Car',
+  author: 'Evan',
+  image: 'http://media.caranddriver.com/images/16q2/667349/2016-honda-civic-coupe-20l-manual-test-review-car-and-driver-photo-668444-s-429x262.jpg',
   votes: 2,
-  description:  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  description:  "Honda"
   }];
+  $scope.upVote = function (post) {
+         post.votes++;
+     };
+     $scope.downVote = function (post) {
+            post.votes--;
+        };
+$scope.started = false;
+
+
+
 });
