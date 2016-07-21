76e313d78ae13bc7fd52c38d65fd1e21cf745a99
rayk216 <raykoren@gmail.com>
2016-07-21 11:34:43 -0600

    post styles and divs setup

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ $scope.posts = [
   {title: 'Cool Car 1',
   author: 'Ray',
   image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
+  votes: 5,
   description:  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 },
   {title: 'Cool Car 2',
   author: 'Ray',
   image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
+  votes: 2,
   description:  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
   }];
 });
