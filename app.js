7805855510d0cb6fa02ae1450bfdc673d38204d2
rayk216 <raykoren@gmail.com>
2016-07-21 16:39:47 -0600

    post working

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var app = angular.module("reddit", []);
-// $scope.sortType = "votes";
+
 app.controller("posts", function($scope){
+$scope.sortType = "votes";
+
 $scope.posts = [
   {title: 'Cool Car',
   author: 'Ray',
   image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
   votes: 5,
-  description:  "AUDI"
+  description:  "AUDI",
+  date: new Date("2016-07-17T16:19:53.155Z")
+},
+{title: 'B Cool Car',
+author: 'Ray',
+image: 'http://autoguide.com.vsassets.com/blog/wp-content/uploads/2016/05/2016-audi-a5.jpg',
+votes: 34,
+description:  "BMW",
+date: new Date("2015-11-17T16:19:53.155Z")
 },
   {title: 'Another Car',
   author: 'Evan',
   image: 'http://media.caranddriver.com/images/16q2/667349/2016-honda-civic-coupe-20l-manual-test-review-car-and-driver-photo-668444-s-429x262.jpg',
   votes: 2,
-  description:  "Honda"
+  description:  "Honda",
+  date: new Date("2016-03-17T16:19:53.155Z")
   }];
   $scope.upVote = function (post) {
          post.votes++;
@@ -20,8 +31,14 @@ $scope.posts = [
      $scope.downVote = function (post) {
             post.votes--;
         };
-$scope.started = false;
-
-
-
+        $scope.submit = function(formData) {
+        $scope.posts.push({
+          title: formData.title,
+          author: formData.author,
+          description: formData.description,
+          image: formData.image,
+          votes: 0,
+          date: new Date
+        });
+  };
 });
